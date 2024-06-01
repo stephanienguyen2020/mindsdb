4f24a14463bb585629c8663cf80918bb273f5932
Kelly Johnson <MaitreyaBuddha@users.noreply.github.com>
2024-06-01 13:55:04 -0700

    Mongo insert/query layer

---
+++ mongo/mongo.py
@@ -0,0 +1,110 @@
+import asyncio
+from typing import List
+
+import together
+from beanie import Document
+from beanie import init_beanie
+from flask import request, jsonify
+from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseModel, Field
+from pymongo import MongoClient
+from quart import Quart
+
+TOGETHER_API_KEY = '62584312b992b79e3e76c031ff115c6a10e72ab9d222a06266b7c2c55a6961e6'
+MONGODB_URI = "mongodb+srv://hackers:aicampers@hackathon.zquobpp.mongodb.net/?retryWrites=true&w=majority&appName=hackathon"
+mongodb_client = MongoClient(MONGODB_URI)
+
+
+async def mongo_insert(content):
+    embeddings = generate_embeddings([content])[0]
+    vector = Vector(content=content, content_embeddings=embeddings)
+    await vector.insert()
+
+
+app = Quart(__name__)
+
+
+@app.before_request
+def initialize():
+    if not hasattr(app, 'db_initialized'):
+        asyncio.run(init_mongo())
+        app.db_initialized = True
+
+
+@app.route('/search', methods=['GET'])
+async def search():
+    query = request.args.get('query')
+    if not query:
+        return jsonify({'error': 'No query provided'}), 400
+
+    results = await search_content(query)
+    return jsonify({'results': results})
+
+
+async def init_mongo():
+    client = AsyncIOMotorClient(MONGODB_URI)
+
+    db = client.Hackathon_DB
+
+    await init_beanie(database=db, document_models=[Vector])
+
+
+def generate_embeddings(input_texts: List[str]) -> List[List[float]]:
+    """Generate embeddings from Together python library.
+
+    Args:
+        input_texts: a list of string input texts.
+        model_api_string: str. An API string for a specific embedding model of your choice.
+
+    Returns:
+        embeddings_list: a list of embeddings. Each element corresponds to the each input text.
+    """
+
+    embedding_model_string = 'togethercomputer/m2-bert-80M-8k-retrieval'
+    together_client = together.Together(api_key=TOGETHER_API_KEY)
+    outputs = together_client.embeddings.create(
+        input=input_texts,
+        model=embedding_model_string,
+    )
+    return [x.embedding + x.embedding for x in outputs.data]
+
+
+async def search_content(content):
+    embeddings = generate_embeddings([content])[0]
+    query = VectorSearchQuery(queryVector=embeddings)
+    agg = [
+        {
+            '$vectorSearch': query.model_dump(),
+        },
+        {
+            '$project': {
+                '_id': 0,
+                'content': 1,
+                'score': {
+                    '$meta': 'vectorSearchScore',
+                },
+            },
+        }
+    ]
+    return await Vector.aggregate(agg).to_list()
+
+
+class VectorView(BaseModel):
+    content: str
+
+
+class Vector(Document):
+    content: str
+    content_embeddings: List[float]
+
+    class Settings:
+        name = "Prompts"
+
+
+class VectorSearchQuery(BaseModel):
+    index: str = Field(default="vector_index")
+    path: str = Field(default="content_embeddings")
+    # filter: Optional[dict] = {}
+    queryVector: List[float]
+    numCandidates: int = Field(default=200)
+    limit: int = Field(default=10)
---
+++ tests/test_mongo.py
@@ -0,0 +1,39 @@
+import pytest
+
+from mongo.mongo import VectorView, Vector, generate_embeddings, mongo_insert, init_mongo, search_content
+
+
+def test_generate_embeddings():
+    print(generate_embeddings('hello'))
+
+
+@pytest.mark.asyncio
+async def test_insert():
+    await init_mongo()
+    num = await Vector.count()
+    await mongo_insert(f'Vector {num}!')
+    print(await Vector.find().project(VectorView).to_list())
+
+
+@pytest.mark.asyncio
+async def test_find():
+    await init_mongo()
+    print(await Vector.find().to_list())
+
+
+@pytest.mark.asyncio
+async def test_search():
+    await init_mongo()
+    results = await search_content('prompt')
+    print(results)
+
+
+@pytest.mark.asyncio
+async def test_agg():
+    await init_mongo()
+    size_aggregate = [{'$sample': {'size': 1}}]
+    return print(await Vector.aggregate(size_aggregate).to_list())
+
+
+if __name__ == '__main__':
+    pytest.main()
